8c8445efd0d195aa736bd5d14227c850091c6833
rupu <abrarkarimrupu1075@gmail.com>
2025-07-18 01:32:23 +0600

    really done

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React from 'react';
 
 const axiosInstance = axios.create({
-    baseURL: `http://localhost:3900`
+    baseURL: `https://a-12-devforum-server.vercel.app`
 })
 
 const useAxios = () => {
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from './useAuth';
 import { useNavigate } from 'react-router';
 
 export const axiosSecure = axios.create({
-    baseURL: `http://localhost:3900`
+    baseURL: `https://a-12-devforum-server.vercel.app`
 })
 
 const useAxiosSecure = () => {
